734cab4d06bcc15d218d32526bf4ca7e8b3ded67
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-01-16 12:00:54 +0000

    Add files via upload

---
+++ Whateverthisitthonny.py
@@ -0,0 +1,26 @@
+'''temp=int(input("Enter value for temperature: " ))
+if temp < 32:
+    print("Ice")
+elif temp < 212:
+    print ("Water")
+else:
+    print ("Steam")
+
+x=1
+if x>3:
+    if x>4:
+        print("A", end=' ')
+    else:
+        print ("B", end=" ")
+elif x<2:
+    if (x != 0):
+        print ("C", end=" ")
+else:
+    print ("D")'''
+
+weather='raining'
+if weather= 'sunny' :
+    print ('Wear sunblock')
+elif weather= ("snow"):
+    print ('going skiing')
+    
