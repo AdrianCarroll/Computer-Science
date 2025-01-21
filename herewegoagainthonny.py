bc75e8f2bc8161856c1af6923ef5cc13ff4da7fc
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-01-21 13:32:20 +0000

    Add files via upload

---
+++ herewegoagainthonny.py
@@ -0,0 +1,30 @@
+"""print ('''
+1
+2
+3
+''')
+
+text= "Test. \nNext line"
+print(text)
+
+print ('One', 'Two' * 2)
+print ('One' + 'Two' * 2)
+print (len('0123456789'))
+
+s = '0123456789'
+print (s[3], ", ", s[0:3], "-", s[2:5])
+print (s[:3], ", ", s[3:], ", ", s[3:100])
+print (s[20:], s[2:1], s[1:1])
+
+s = '987654321'
+print (s[-1], s[-3])
+print (s[-3], s[-3])
+print (s[-100:-3], s[-100:3])
+
+x= "hello" * 3
+y= str(123)
+print (x, y)
+x= "Hello" + "World"
+y= len(x)
+print (y, x)"""
+
