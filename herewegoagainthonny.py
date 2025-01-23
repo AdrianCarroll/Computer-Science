13bb07cf852f7a430ec2fc40e2f92599a8214d94
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-01-23 11:37:14 +0000

    Add files via upload

--- herewegoagainthonny.py
+++ herewegoagainthonny.py
@@ -65,7 +65,7 @@ while count <= inputInt:
 print(newStr)
 print(testStr)
 print(count)
-print(inputInt)"""
+print(inputInt)
 
 app=input("Give me a string: ")
 big=0
@@ -82,3 +82,55 @@ print(big)
 print(lil)
 print(other)
 print(app.isdigit())
+
+D1=input("Enter string of digits")
+D2=input("Enter string of digits")
+if len(D1)>len(D2):
+    small=D2
+    large=D1
+else:
+    small=D1
+    large=D2
+new=' '
+for element in small:
+    result=int(element)+int(large[0])
+    new=new+str(result)
+    large=large[1:]
+print(len(new))
+print(new)
+print(large)
+print(small)
+
+S=input("Enter string: ")
+RS= ' '
+for ch in S:
+    RS=ch+RS
+print(S+RS)
+
+S=input("Enter string: ")
+RS= ' '
+for ch in S:
+    RS=ch+2+RS
+print(RS+S)
+
+s="PURA VIDA"
+print(s[9] + s[9:15])
+
+s="PURA VIDA"
+s1=(s[:10] + s[10:])
+s2=(s[10]+s[-10])
+
+s="PURA VIDA"
+s1=s*2
+s2=s1[-19] - s1[-20]
+s3= s1[-19:]
+
+s="PURA VIDA"
+s1=s[:5]
+s2=s[5:]
+s3=s1*s2
+s4=s2+'3'
+s5=s3+3
+
+"whenever".find("never")
+"whenever".find("what")"""
