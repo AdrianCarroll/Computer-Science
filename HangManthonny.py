dde0f82ea41afa12941df74ebeab93be4cee5bf7
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-01-28 13:40:52 +0000

    Add files via upload

--- HangManthonny.py
+++ HangManthonny.py
@@ -1,10 +1,10 @@
-secret="apple"
+secret="banana"
 print("Welcome to Hangman! you have 7 lives")
 lives=7
 attempt= "_____"
 while "_" in attempt:
     go=input("Enter a letter/guess (must be lowercase): ")
-    if go=="apple":
+    if go=="banana":
         print("Win!")
         break
     elif go in secret:
@@ -14,8 +14,15 @@ while "_" in attempt:
             attempt = attempt[0:indexPos]+go+attempt[indexPos+1:]
             print(attempt)
         else:
-            indexPos = secret.find(go)
-            attempt = attempt[0:indexPos]+go+go+attempt[indexPos+1:]
+            oldindex=0
+            start=0
+            for i in range (numletter):
+                indexPos = secret[start:].find(go)
+                print (indexPos)
+                attempt = attempt[0:indexPos+start]+go+attempt[indexPos+oldindex+2:]
+                print (attempt)
+                oldindex=indexPos
+                start=indexPos+1
             print(attempt)
     else:
         print("Wrong guess!")
