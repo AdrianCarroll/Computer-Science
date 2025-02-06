f2378e56d3facc36c4da6d309e0b7cbf2801e81f
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-02-06 12:03:39 +0000

    Add files via upload

--- HangManthonny.py
+++ HangManthonny.py
@@ -1,3 +1,52 @@
+string1='''
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+'''
+string2='''
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+'''
+string3='''
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+'''
+string4='''
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+'''
+string5='''
+ +---+
+ |   |
+ O   |
+ |   |
+     |
+'''
+string6='''
+ +---+
+ |   |
+ O   |
+     |
+     |
+'''
+string7='''
+ +---+
+ |   |
+     |
+     |
+     |
+'''
 again="yes"
 while "yes" in again:
         secret="bat"
@@ -5,15 +54,17 @@ while "yes" in again:
         lives=7
         x_tmp = secret
         listt=''
-        go=input("Enter a letter/guess (must be lowercase): ")
+        go=input("Enter a letter/guess: ")
+        go=go.lower()
         while go in listt:
             print("Letter already used")
-            go=input("Enter a letter/guess (must be lowercase): ")
+            go=input("Enter a letter/guess: ")
+            go=go.lower()
         listt=listt+go
         attempt= "_"*len(secret)
         index = secret.find(go)
         start = 0
-        print("Welcome back to Hangman! you have 7 lives")
+        '''print("Welcome back to Hangman! you have 7 lives")'''
         while "_" in attempt:
             if go in secret:
                 listt=listt+go
@@ -32,11 +83,24 @@ while "yes" in again:
             else:
                 print("Wrong guess!")
                 lives=lives-1
-                if lives<=0:
-                   print("Out of lives! Game over")
-                   break
+                if lives==6:
+                   print(string7)
+                elif lives==5:
+                   print(string6)
+                elif lives==4:
+                   print(string5)
+                elif lives==3:
+                   print(string4)
+                elif lives==2:
+                   print(string3)
+                elif lives==1:
+                   print(string2)
+                else lives==0:
+                   print(string1)
+                   print("Game over")
             if attempt==secret:
                 print("Win!")
                 break
-            go=input("Enter a letter/guess (must be lowercase): ")
+            go=input("Enter a letter/guess: ")
+            go=go.lower()
         again=input("Would you like to play again?")
