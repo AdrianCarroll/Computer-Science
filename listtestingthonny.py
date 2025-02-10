7ba6fd3c65de32f1b25509a5433b1de571bef95b
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-02-10 13:38:37 +0000

    Add files via upload

---
+++ listtestingthonny.py
@@ -0,0 +1,23 @@
+a=[]
+b=[1,2,3]
+c=[1, 2.5, 3.7, 9]
+d=["a", "b", "c"]
+e=["a", 1, "b", 3.7, "zero"]
+
+f=[2, 4, 6]
+g=["abc", "cba"]
+h=[1, 2.0, 3, 4.0]
+
+long=["beep", "Glorp",
+      1]
+
+nest=[1, 2, [3, 4], 5, 6]
+
+L=list("greetings")
+
+golden=["coins", "bars", "apples"]
+gold=golden[1]
+
+HD=["N", "E", "O", "N"]
+for i in HD:
+    print(i)
