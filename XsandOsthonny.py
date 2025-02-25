fc188546021283cf10f4857bcb5a8ac1b3d1c15e
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-02-25 13:29:33 +0000

    Add files via upload

--- XsandOsthonny.py
+++ XsandOsthonny.py
@@ -7,30 +7,61 @@ print("To win, create a line with X's or O's. The X player always goes first")
 print(grid1)
 print(grid2)
 print(grid3)
-for a in grid1 and grid2 or grid3:
-    row=int(input("Enter a number from 1 to 3: "))
-    collum=int(input("Enter a 0, 2 or 4: "))
-    if row==1:
-        if collum==0:
-            grid1[0]='X'
-        elif collum==2:
-            grid1[2]='X'
+player=1
+while a in grid1 and grid2 or grid3:
+    if player==1:
+        row=int(input("Enter a number from 1 to 3: "))
+        collum=int(input("Enter a 0, 2 or 4: "))
+        if row==1:
+            if collum==0:
+                grid1[0]='X'
+            elif collum==2:
+                grid1[2]='X'
+            else:
+                grid1[4]='X'
+        elif row==2:
+            if collum==0:
+                grid2[0]='X'
+            elif collum==2:
+                grid2[2]='X'
+            else:
+                grid2[4]='X'
         else:
-            grid1[4]='X'
-    elif row==2:
-        if collum==0:
-            grid2[0]='X'
-        elif collum==2:
-            grid2[2]='X'
-        else:
-            grid2[4]='X'
+            if collum==0:
+                grid3[0]='X'
+            elif collum==2:
+                grid3[2]='X'
+            else:
+                grid3[4]='X'
+        print(grid1)
+        print(grid2)
+        print(grid3)
+        player=2
     else:
-        if collum==0:
-            grid3[0]='X'
-        elif collum==2:
-            grid3[2]='X'
+        row=int(input("Enter a number from 1 to 3: "))
+        collum=int(input("Enter a 0, 2 or 4: "))
+        if row==1:
+            if collum==0:
+                grid1[0]='O'
+            elif collum==2:
+                grid1[2]='O'
+            else:
+                grid1[4]='O'
+        elif row==2:
+            if collum==0:
+                grid2[0]='O'
+            elif collum==2:
+                grid2[2]='O'
+            else:
+                grid2[4]='O'
         else:
-            grid3[4]='X'
-    print(grid1)
-    print(grid2)
-    print(grid3)
+            if collum==0:
+                grid3[0]='O'
+            elif collum==2:
+                grid3[2]='O'
+            else:
+                grid3[4]='O'
+        print(grid1)
+        print(grid2)
+        print(grid3)
+        player=1
