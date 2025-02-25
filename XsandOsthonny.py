04a0be77fc1c09a6b3a65ed6f43dd8c17c557fcf
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-02-25 12:56:02 +0000

    Add files via upload

--- XsandOsthonny.py
+++ XsandOsthonny.py
@@ -12,4 +12,25 @@ for a in grid1 and grid2 or grid3:
     collum=int(input("Enter a 0, 2 or 4: "))
     if row==1:
         if collum==0:
-            
+            grid1[0]='X'
+        elif collum==2:
+            grid1[2]='X'
+        else:
+            grid1[4]='X'
+    elif row==2:
+        if collum==0:
+            grid2[0]='X'
+        elif collum==2:
+            grid2[2]='X'
+        else:
+            grid2[4]='X'
+    else:
+        if collum==0:
+            grid3[0]='X'
+        elif collum==2:
+            grid3[2]='X'
+        else:
+            grid3[4]='X'
+    print(grid1)
+    print(grid2)
+    print(grid3)
