7b88df0a773f048fd4636dba3e03d4ac67d27c3a
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-02-26 11:48:21 +0000

    Add files via upload

---
+++ connect4thonny.py
@@ -0,0 +1,15 @@
+a="_"
+row1=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+row2=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+row3=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+row4=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+row5=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+row6=["|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|", a, "|"]
+print("Welcome to connect 4")
+print("To win, make a vertical, horizontal or diagonal line using R or Y. R goes first: ")
+print(row1)
+print(row2)
+print(row3)
+print(row4)
+print(row5)
+print(row6)
