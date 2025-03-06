38a388e2de1017df989bcb86be306665620412f4
AdrianCarroll <24ACarrollMiguel.ACC@lwetb.ie>
2025-03-06 11:59:03 +0000

    Add files via upload

--- connect4thonny.py
+++ connect4thonny.py
@@ -16,17 +16,17 @@ print(row6)
 while a in row1:
     if a in row6:
         collum=input("Pick a number from 1 to 7: ")
-        if collum==1:
+        if collum=='1':
             row6[1]="R"
-        elif collum==2:
+        elif collum=='2':
             row6[3]="R"
-        elif collum==3:
+        elif collum=='3':
             row6[5]="R"
-        elif collum==4:
+        elif collum=='4':
             row6[7]="R"
-        elif collum==5:
+        elif collum=='5':
             row6[9]="R"
-        elif collum==6:
+        elif collum=='6':
             row6[11]="R"
         else:
             row6[13]="R"
@@ -38,20 +38,42 @@ while a in row1:
         print(row6)
     elif a in row5:
         collum=input("Pick a number from 1 to 7: ")
-        if collum==1:
+        if collum=='1':
             row5[1]="R"
-        elif collum==2:
+        elif collum=='2':
             row5[3]="R"
-        elif collum==3:
+        elif collum=='3':
             row5[5]="R"
-        elif collum==4:
+        elif collum=='4':
             row5[7]="R"
-        elif collum==5:
+        elif collum=='5':
             row5[9]="R"
-        elif collum==6:
+        elif collum=='6':
             row5[11]="R"
         else:
-            row5[13]=="R"
+            row5[13]="R"
+        print(row1)
+        print(row2)
+        print(row3)
+        print(row4)
+        print(row5)
+        print(row6)
+    elif a in row4:
+        collum=input("Pick a number from 1 to 7: ")
+        if collum=='1':
+            row4[1]="R"
+        elif collum=='2':
+            row4[3]="R"
+        elif collum=='3':
+            row4[5]="R"
+        elif collum=='4':
+            row4[7]="R"
+        elif collum=='5':
+            row4[9]="R"
+        elif collum=='6':
+            row4[11]="R"
+        else:
+            row4[13]="R"
         print(row1)
         print(row2)
         print(row3)
